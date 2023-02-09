974aa3fabeb2cac6525f8e8934073cea7168f91e
evgKireev <bob1_k@mail.ru>
2023-02-09 23:53:56 +0300

    cors

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-
+app.use(
+  cors({
+    credentials: true,
+    origin: ['https://chat-backend-production.up.railway.app'],
+  })
+)
 const rooms = new Map()
 
 app.get('/rooms/:id', (req, res) => {
